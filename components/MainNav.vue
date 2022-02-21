674311a77e0687296d5d3b32e3daec9570f09c55
Daniel Rossner <daniel.rossner@posteo.de>
2022-02-21 14:57:53 +0100

    init with ignore

---
+++ components/MainNav.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "MainNav"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Question.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Question"
+}
+</script>
+
+<style scoped>
+
+</style>
