a2a0d4edf735c80602f1a847dd30b3802dbd5634
Daniel Rossner <daniel.rossner@posteo.de>
2022-02-23 15:37:52 +0100

    enabled iOS pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,13 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'openfischer',
+    title: 'OpenFischer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { mobileAppIOS: true }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
