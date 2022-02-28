2b4e4021ea90f81cfcfa102142aaf793a778a066
Daniel Rossner <daniel.rossner@posteo.de>
2022-02-28 19:27:50 +0100

    reworked overview

--- pages/overview.vue
+++ pages/overview.vue
@@ -1,12 +1,24 @@
 <template>
   <MainNav>
-    <b-row>
+    <b-row class="mb-3">
+      <b-col>
+        <b-input-group>
+          <b-form-input placeholder="Fragen durchsuchen" v-model="enteredText" @input="startQuery"></b-form-input>
+        </b-input-group>
+      </b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col>
+        <b-pagination v-model="currentPage"  per-page="pageLimit" :total-rows="allData.length" use-router></b-pagination>
+      </b-col>
+    </b-row>
+    <b-row class="mb-2">
       <b-col>
         <b-list-group>
-          <b-list-group-item v-for="qst in qstDatas" :key="qst.id" :variant="qst.variant">
-            <NuxtLink :to='"/catalog/"+qst.id'>{{ qst.id }}</NuxtLink>
-          </b-list-group-item>
-        </b-list-group>
+                  <b-list-group-item v-for="qst in shownData" :key="qst.id" :variant="qst.variant" :to='"/catalog/"+qst.id'>
+                    <b>{{ qst.id }}</b> {{ qst.question  }}
+                  </b-list-group-item>
+                </b-list-group>
       </b-col>
     </b-row>
   </MainNav>
@@ -17,29 +29,46 @@ export default {
   name: 'Overview',
   data: function () {
     return {
-      qstDatas: []
+      allData: [],
+      pageLimit: 12,
+      enteredText: "",
+      currentPage: 1
     }
   },
   async fetch() {
-    let questIds = await this.$content('catalog').only(['id']).fetch()
-    const keys = await this.$localForage.keys()
-    for(let qst of questIds) {
-      if(keys.includes(qst.id)) {
-        let save =  await this.$localForage.getItem(qst.id)
-        let variant;
-        if(save.correct === true)
-          variant = "success";
-        else if(save.correct === false)
-          variant = "danger"
-        this.qstDatas.push({id: qst.id, variant: variant})
-      } else {
-        this.qstDatas.push({id: qst.id})
-      }
-
+    let query = this.$route.query.term || ""
+    this.allData = await this.$content('catalog').where({question: {$regex: query}}).fetch();
+  },
+  methods: {
+    linkGen(pageNum) {
+      return pageNum === 1 ? '?' : `?page=${pageNum}`
+    },
+    startQuery() {
+      let query = this.enteredText
+      this.$content('catalog').where({question: {$regex: query}}).fetch().then(res => this.allData = res)
     }
   },
   computed: {
+    shownData: function () {
+      let beginSlice = (this.currentPage - 1) * this.pageLimit
+      let result =  this.allData.slice(beginSlice, beginSlice + this.pageLimit)
+      for(let qst of result) {
+        let save = this.$store.state.questionLocal[qst.id]
+        if(save === undefined || save === null){
 
+        } else {
+          if(save.correct){
+            qst.variant = "success"
+          }
+          else
+            qst.variant = "danger"
+        }
+      }
+      return result
+    },
+    numberOfPages: function () {
+      return Math.ceil(this.allData.length / this.pageLimit)
+    }
   }
 }
 </script>
